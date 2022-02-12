d15801dffdf70aa23f16e78548a2250f26696342
Tsvetkov HomePC <dstvetkov@gmail.com>
2022-02-13 01:02:38 +0300

    - Разбил разметку на компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/mainScreen';
+import Header from '../header/header';
+
+type AppMainProps = {
+  placesCount: number,
+}
+
+function App({placesCount}: AppMainProps): JSX.Element {
+  return (
+    <div className="page">
+      <Header/>
+      <MainScreen placesCount = {placesCount}/>
+    </div>
+  );
 }
 
 export default App;
